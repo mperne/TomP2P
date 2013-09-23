f8fa53f0fc8554bf8a32375d3fe6d3efca5b6478
Maxat <m.pernebayev@gmail.com>
2013-09-23 18:53:45 +0200

    synced with upstream

--- core/src/main/java/net/tomp2p/connection2/PeerCreator.java
+++ core/src/main/java/net/tomp2p/connection2/PeerCreator.java
@@ -140,6 +140,13 @@ public class PeerCreator {
         }
         // de-register in dispatcher
         connectionBean.dispatcher().removeIoHandler(peerBean().serverPeerAddress().getPeerId());
+        // shutdown running tasks for this peer
+        if(peerBean.maintenanceTask() != null) {
+            peerBean.maintenanceTask().cancel();
+        }
+        if(peerBean.replicationExecutor()!=null) {
+            peerBean.replicationExecutor().cancel();
+        }
         // shutdown all children
         if (!master) {
             for (PeerCreator peerCreator : childConnections) {
@@ -147,6 +154,8 @@ public class PeerCreator {
             }
             return shutdownFuture().setDone();
         }
+        //shutdown the timer
+        connectionBean.timer().cancel();
         // we have two things to shut down: the server that listens for incoming connections and the connection creator
         // that establishes connections
         final int maxListeners = 2;
