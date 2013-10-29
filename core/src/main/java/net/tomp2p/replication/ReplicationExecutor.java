172e276979b10fecd416016e7e78b848877203e3
Maxat <m.pernebayev@gmail.com>
2013-10-29 15:43:04 +0100

    commit

--- core/src/main/java/net/tomp2p/replication/ReplicationExecutor.java
+++ core/src/main/java/net/tomp2p/replication/ReplicationExecutor.java
@@ -16,31 +16,24 @@
 
 package net.tomp2p.replication;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.SortedSet;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import net.tomp2p.connection2.ChannelCreator;
 import net.tomp2p.futures.BaseFutureAdapter;
 import net.tomp2p.futures.FutureChannelCreator;
+import net.tomp2p.futures.FutureDone;
 import net.tomp2p.futures.FuturePut;
 import net.tomp2p.futures.FutureResponse;
-import net.tomp2p.message.Buffer;
-import net.tomp2p.message.DataMap;
-import net.tomp2p.message.Message2;
-import net.tomp2p.message.Message2.Type;
 import net.tomp2p.p2p.Peer;
-import net.tomp2p.p2p.RequestP2PConfiguration;
-import net.tomp2p.p2p.builder.DHTBuilder;
 import net.tomp2p.p2p.builder.PutBuilder;
-import net.tomp2p.p2p.builder.SynchronizationBuilder;
+import net.tomp2p.p2p.builder.SynchronizationStatistics;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.peers.Number480;
 import net.tomp2p.peers.PeerAddress;
@@ -69,11 +62,14 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
 
     private final Replication replicationStorage;
     // default replication for put and add is 6
-    private static final int REPLICATION = 2;
+    private static final int REPLICATION = 6;
     
-    private int intervalMillis = 60 * 1000;
+    private final Timer timer;
+    private final Random random;
     
-    private Synchronization synchronization;
+    private final int delayMillis;
+    
+//    private Synchronization synchronization;
     private AutomaticReplication automaticReplication;
 
     /**
@@ -82,31 +78,42 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
      * @param peer
      *            The peer
      */
-    public ReplicationExecutor(final Peer peer) {
+    public ReplicationExecutor(final Peer peer, final Random random, final Timer timer, final int delayMillis) {
         this.peer = peer;
         this.storage = peer.getPeerBean().storage();
         this.storageRPC = peer.getStoreRPC();
         this.replicationStorage = peer.getPeerBean().replicationStorage();
         replicationStorage.addResponsibilityListener(this);
         replicationStorage.setReplicationFactor(REPLICATION);
+        this.random = random;
+        this.timer = timer;
+        this.delayMillis = delayMillis;
         
-        this.synchronization = new Synchronization();
+//        this.synchronization = new Synchronization();
         this.automaticReplication = new AutomaticReplication(0.999999, peer.getPeerBean().peerMap());
     }
     
-    public void init(Peer peer, Timer timer) {
+    public void init(Peer peer, int intervalMillis) {
         timer.scheduleAtFixedRate(this, intervalMillis, intervalMillis);
     }
 
     @Override
-    public void otherResponsible(final Number160 locationKey, final PeerAddress other) {
+    public void otherResponsible(final Number160 locationKey, final PeerAddress other, final boolean delayed) {
         LOG.debug("Other peer {} is responsible for {}. I'm {}", other, locationKey, storageRPC.peerBean()
                 .serverPeerAddress());
-
-        final Map<Number480, Data> dataMap = storage.subMap(locationKey);
-        sendDirect(other, locationKey, dataMap);
-        LOG.debug("transfer from {} to {} for key {}", storageRPC.peerBean().serverPeerAddress(), other,
-                locationKey);        
+        if(!delayed) {
+            final Map<Number480, Data> dataMap = storage.subMap(locationKey);
+            sendDirect(other, locationKey, dataMap);
+            LOG.debug("transfer from {} to {} for key {}", storageRPC.peerBean().serverPeerAddress(), other,
+                    locationKey);
+        } else {
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    otherResponsible(locationKey, other, false);
+                }
+            }, random.nextInt(delayMillis));
+        }
     }
 
     @Override
@@ -117,7 +124,6 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
 
     @Override
     public void run() {
-    	//System.out.println(peer.getPeerID()+ " run");
         // we get called every x seconds for content we are responsible for. So
         // we need to make sure that there are enough copies. The easy way is to
         // publish it again... The good way is to do a diff
@@ -129,6 +135,7 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
         
         int replicationFactor = automaticReplication.getReplicationFactor(peer.getPeerID());
 		peer.getPeerBean().replicationStorage().setReplicationFactor(replicationFactor);
+        
     }
 
     /**
@@ -138,7 +145,6 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
      *            The location key.
      */
     private void synchronizeData(final Number160 locationKey) {
-    	//System.out.println(peer.getPeerID()+" synchronizeData");
         final Map<Number480, Data> dataMap = storage.subMap(locationKey);
         Number160 domainKeyOld = null;
         Map<Number160, Data> dataMapConverted = new HashMap<Number160, Data>();
@@ -164,27 +170,6 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
             peer.notifyAutomaticFutures(futurePut);
         }
     }
-    
-    /**
-     * If my peer is responsible, I'll issue a put if absent to make sure all replicas are stored.
-     * 
-     * @param locationKey
-     *            The location key
-     * @param domainKey
-     *            The domain key
-     * @param dataMapConverted
-     *            The data to store
-     * @return The future of the put
-     */
-    protected FuturePut send1(final Number160 locationKey, final Number160 domainKey,
-            final Map<Number160, Data> dataMapConverted) {
-    	System.out.println(peer.getPeerID()+" send");
-    	RequestP2PConfiguration requestP2PConfiguration = new RequestP2PConfiguration(replicationStorage.getReplicationFactor(), 10, 0 );
-    	//RequestP2PConfiguration requestP2PConfiguration = new RequestP2PConfiguration(1, 10, 0 );
-    	return peer.put(locationKey).setDataMapContent(dataMapConverted).setDomainKey(domainKey).setRequestP2PConfiguration(requestP2PConfiguration).setPutIfAbsent(true).start();
-        //return peer.put(locationKey).setDataMapContent(dataMapConverted).setDomainKey(domainKey)
-        //        .setPutIfAbsent(true).start();
-    }
 
     /**
      * If my peer is responsible, I'll issue a put if absent to make sure all replicas are stored.
@@ -196,10 +181,10 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
      * @param dataMapConverted
      *            The data to store
      * @return The future of the put
-     */    
+     */
     protected FuturePut send(final Number160 locationKey, final Number160 domainKey,
             final Map<Number160, Data> dataMapConverted) {
-		int replicationFactor = 6;//replicationStorage.getReplicationFactor();		
+		int replicationFactor = replicationStorage.getReplicationFactor();		
 		replicationFactor--;
 		List<PeerAddress> closePeers = new ArrayList<PeerAddress>();
     	SortedSet<PeerAddress> sortedSet = peer.getPeerBean().peerMap().closePeers(locationKey, replicationFactor);
@@ -212,7 +197,7 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
     	}
 
     	return null;
-    }    
+    }
     
     /**
      * Extracts each content-value pair from the same location and domain keys
@@ -229,16 +214,20 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
     public void extractEachData(final PeerAddress other, final Number160 locationKey, final Number160 domainKey, final Map<Number160, Data> dataMapConverted){
     	for (Map.Entry<Number160, Data> entry : dataMapConverted.entrySet()) {
     		Number160 contentKey=entry.getKey();
-			Data data = entry.getValue();
-			Map<Number160, Data> dataMapConverted1 = new HashMap<Number160, Data>();
-			dataMapConverted1.put(contentKey, data);
-			try {
-				checkDirect(other, locationKey, domainKey, contentKey, data, dataMapConverted1);				
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+//			Data data = entry.getValue();
+//			Map<Number160, Data> dataMapConverted1 = new HashMap<Number160, Data>();
+//			dataMapConverted1.put(contentKey, data);
+//			try {
+//				checkDirect(other, locationKey, domainKey, contentKey, data, dataMapConverted1);				
+//			} catch (ClassNotFoundException e) {
+//				e.printStackTrace();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+			
+			Number480 key = new Number480(locationKey, domainKey, contentKey);
+			FutureDone<SynchronizationStatistics> future = peer.synchronize(other).key(key).start();
+            future.awaitUninterruptibly();
     	}    	
     }
     
@@ -260,50 +249,50 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
      * @throws ClassNotFoundException
      * @throws IOException
      */
-    public void checkDirect(final PeerAddress other, final Number160 locationKey, final Number160 domainKey, final Number160 contentKey, final Data data, final Map<Number160, Data> dataMapConverted) throws ClassNotFoundException, IOException {	
-    	final String value = data.object().toString();
-    	
-        FutureChannelCreator futureChannelCreator = peer.getConnectionBean().reservation().create(0, 2);
-        futureChannelCreator.addListener(new BaseFutureAdapter<FutureChannelCreator>() {
-            @Override
-            public void operationComplete(final FutureChannelCreator future2) throws Exception {
-                if (future2.isSuccess()) {
-                	SynchronizationBuilder synchronizationBuilder = new SynchronizationBuilder(peer, locationKey, domainKey, contentKey, Number160.createHash(value));
-                	final FutureResponse futureResponse = peer.getSynchronizationRPC().infoMessage(other, synchronizationBuilder, future2.getChannelCreator());
-                	futureResponse.addListener(new BaseFutureAdapter<FutureResponse>() {
-						@Override
-						public void operationComplete(FutureResponse future) throws Exception {
-							if(future.isFailed()) {
-								Utils.addReleaseListener(future2.getChannelCreator(), futureResponse);
-								System.out.println("Failed: "+future.getFailedReason());
-								return;
-							}
-							Message2 responseMessage = future.getResponse();
-							if(responseMessage.getType() == Type.OK){
-								System.out.println("SAME: "+responseMessage.getRecipient().getPeerId()+" <- "+responseMessage.getSender().getPeerId());// + " " +responseMessage);
-								Utils.addReleaseListener(future2.getChannelCreator(), futureResponse);
-							}
-							else if(responseMessage.getType() == Type.NOT_FOUND) {
-								System.out.println("NO: "+responseMessage.getRecipient().getPeerId()+" <- "+responseMessage.getSender().getPeerId());// + " " +responseMessage);
-								FutureResponse fr = copy(future2.getChannelCreator(), responseMessage.getSender(), locationKey, domainKey, dataMapConverted);
-								Utils.addReleaseListener(future2.getChannelCreator(), fr, futureResponse);
-							} 
-							else if(responseMessage.getType() == Type.PARTIALLY_OK) {
-								System.out.println("NOT_SAME: "+responseMessage.getRecipient().getPeerId()+" <- "+responseMessage.getSender().getPeerId());// + " " +responseMessage);
-								FutureResponse fr = sync(future2.getChannelCreator(), responseMessage.getSender(), locationKey, domainKey, contentKey, value, responseMessage.getBuffer(0));
-								Utils.addReleaseListener(future2.getChannelCreator(), fr, futureResponse);
-							}
-						}
-                	});
-                    peer.notifyAutomaticFutures(futureResponse);
-                } else {
-                    if (LOG.isErrorEnabled()) {
-                        LOG.error("checkDirect failed " + future2.getFailedReason());
-                    }
-                }
-            }
-        });
-    }    
+//    public void checkDirect(final PeerAddress other, final Number160 locationKey, final Number160 domainKey, final Number160 contentKey, final Data data, final Map<Number160, Data> dataMapConverted) throws ClassNotFoundException, IOException {	
+//    	final String value = data.object().toString();
+//    	
+//        FutureChannelCreator futureChannelCreator = peer.getConnectionBean().reservation().create(0, 2);
+//        futureChannelCreator.addListener(new BaseFutureAdapter<FutureChannelCreator>() {
+//            @Override
+//            public void operationComplete(final FutureChannelCreator future2) throws Exception {
+//                if (future2.isSuccess()) {
+//                	SynchronizationBuilder synchronizationBuilder = new SynchronizationBuilder(peer, locationKey, domainKey, contentKey, Number160.createHash(value));
+//                	final FutureResponse futureResponse = peer.getSynchronizationRPC().infoMessage(other, synchronizationBuilder, future2.getChannelCreator());
+//                	futureResponse.addListener(new BaseFutureAdapter<FutureResponse>() {
+//						@Override
+//						public void operationComplete(FutureResponse future) throws Exception {
+//							if(future.isFailed()) {
+//								Utils.addReleaseListener(future2.getChannelCreator(), futureResponse);
+//								System.out.println("Failed: "+future.getFailedReason());
+//								return;
+//							}
+//							Message2 responseMessage = future.getResponse();
+//							if(responseMessage.getType() == Type.OK){
+//								System.out.println("SAME: "+responseMessage.getRecipient().getPeerId()+" <- "+responseMessage.getSender().getPeerId());// + " " +responseMessage);
+//								Utils.addReleaseListener(future2.getChannelCreator(), futureResponse);
+//							}
+//							else if(responseMessage.getType() == Type.NOT_FOUND) {
+//								System.out.println("NO: "+responseMessage.getRecipient().getPeerId()+" <- "+responseMessage.getSender().getPeerId());// + " " +responseMessage);
+//								FutureResponse fr = copy(future2.getChannelCreator(), responseMessage.getSender(), locationKey, domainKey, dataMapConverted);
+//								Utils.addReleaseListener(future2.getChannelCreator(), fr, futureResponse);
+//							} 
+//							else if(responseMessage.getType() == Type.PARTIALLY_OK) {
+//								System.out.println("NOT_SAME: "+responseMessage.getRecipient().getPeerId()+" <- "+responseMessage.getSender().getPeerId());// + " " +responseMessage);
+//								FutureResponse fr = sync(future2.getChannelCreator(), responseMessage.getSender(), locationKey, domainKey, contentKey, value, responseMessage.getBuffer(0));
+//								Utils.addReleaseListener(future2.getChannelCreator(), fr, futureResponse);
+//							}
+//						}
+//                	});
+//                    peer.notifyAutomaticFutures(futureResponse);
+//                } else {
+//                    if (LOG.isErrorEnabled()) {
+//                        LOG.error("checkDirect failed " + future2.getFailedReason());
+//                    }
+//                }
+//            }
+//        });
+//    }    
     
     /**
      * Transferring whole data
@@ -320,13 +309,13 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
      * 			The data to store
      * @return	The future response to keep track of future events
      */
-    public FutureResponse copy(final ChannelCreator cc, final PeerAddress other, final Number160 locationKey, final Number160 domainKey, final Map<Number160, Data> dataMapConverted){
-		final DataMap dataMap = new DataMap(locationKey, domainKey, dataMapConverted);
-        SynchronizationBuilder sb = new SynchronizationBuilder(peer, dataMap);
-        FutureResponse futureResponse = peer.getSynchronizationRPC().copyMessage(other, sb, cc);
-        peer.notifyAutomaticFutures(futureResponse);
-        return futureResponse;
-    }
+//    public FutureResponse copy(final ChannelCreator cc, final PeerAddress other, final Number160 locationKey, final Number160 domainKey, final Map<Number160, Data> dataMapConverted){
+//		final DataMap dataMap = new DataMap(locationKey, domainKey, dataMapConverted);
+//        SynchronizationBuilder sb = new SynchronizationBuilder(peer, dataMap);
+//        FutureResponse futureResponse = peer.getSynchronizationRPC().copyMessage(other, sb, cc);
+//        peer.notifyAutomaticFutures(futureResponse);
+//        return futureResponse;
+//    }
     
     /**
      * Transferring only changed part of data
@@ -350,15 +339,15 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
      * @throws IOException
      * @throws NoSuchAlgorithmException
      */
-    public FutureResponse sync(final ChannelCreator cc, final PeerAddress other, final Number160 locationKey, final Number160 domainKey, final Number160 contentKey, final String value, final Buffer buffer) throws ClassNotFoundException, IOException, NoSuchAlgorithmException{
-		Object object = synchronization.getObject(buffer);
-		ArrayList<Checksum> checksums = (ArrayList<Checksum>) object;
-		ArrayList<Instruction> instructions = synchronization.getInstructions(value.getBytes(), checksums, Synchronization.SIZE);
-		SynchronizationBuilder synchronizationBuilder = new SynchronizationBuilder(peer, locationKey, domainKey, contentKey, Number160.createHash(value), instructions);
-		FutureResponse futureResponse = peer.getSynchronizationRPC().syncMessage(other, synchronizationBuilder, cc);
-        peer.notifyAutomaticFutures(futureResponse);
-        return futureResponse;
-    }
+//    public FutureResponse sync(final ChannelCreator cc, final PeerAddress other, final Number160 locationKey, final Number160 domainKey, final Number160 contentKey, final String value, final Buffer buffer) throws ClassNotFoundException, IOException, NoSuchAlgorithmException{
+//		Object object = synchronization.getObject(buffer);
+//		ArrayList<Checksum> checksums = (ArrayList<Checksum>) object;
+//		ArrayList<Instruction> instructions = synchronization.getInstructions(value.getBytes(), checksums, Synchronization.SIZE);
+//		SynchronizationBuilder synchronizationBuilder = new SynchronizationBuilder(peer, locationKey, domainKey, contentKey, Number160.createHash(value), instructions);
+//		FutureResponse futureResponse = peer.getSynchronizationRPC().syncMessage(other, synchronizationBuilder, cc);
+//        peer.notifyAutomaticFutures(futureResponse);
+//        return futureResponse;
+//    }
 
     /**
      * If an other peer is responsible, we send this peer our data, so that the other peer can take care of this.
@@ -373,7 +362,6 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
      *            The data to store
      */
     protected void sendDirect(final PeerAddress other, final Number160 locationKey, final Map<Number480, Data> dataMap) {
-    	//System.out.println(peer.getPeerID()+" sendDirect");
         FutureChannelCreator futureChannelCreator = peer.getConnectionBean().reservation().create(0, 1);
         futureChannelCreator.addListener(new BaseFutureAdapter<FutureChannelCreator>() {
             @Override
@@ -392,13 +380,5 @@ public class ReplicationExecutor extends TimerTask implements ResponsibilityList
                 }
             }
         });
-    }
-    
-    public int getIntervalMillis() {
-        return intervalMillis;
-    }
-
-    public void setIntervalMillis(int intervalMillis) {
-        this.intervalMillis = intervalMillis;
     }
 }
